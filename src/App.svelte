a8a86b1a3ee60ffb9e9c2966723080989ebf2c33
Deepak Ramalingam <rdeepak2002@gmail.com>
2022-04-05 23:21:23 -0400

    Improve UI by centering elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,198 +1,249 @@
 <script lang="ts">
-	import axios from "axios";
+    import axios from "axios";
 
-	console.log("Init App.svelte");
+    console.log("Init App.svelte");
 
-	let isProd = location.protocol === 'https:';
-	let serverUrl = isProd ? "" : "http://localhost:3000";
-	let imageUrl = `${serverUrl}/static/image.png`;
-	let xInput = 0;
-	let yInput = 0;
-	let redInput = 0;
-	let greenInput = 0;
-	let blueInput = 0;
-	let user: {id: string, name: string, imageUrl: string, email: string, id_token: string} = undefined;
-	let loadingAuthState = true;
-	let expirationTime = undefined;
-	let currentTime = Math.floor(Date.now() / 1000);
+    let isProd = location.protocol === 'https:';
+    let serverUrl = isProd ? "" : "http://localhost:3000";
+    let imageUrl = `${serverUrl}/static/image.png`;
+    let xInput = 0;
+    let yInput = 0;
+    let redInput = 0;
+    let greenInput = 0;
+    let blueInput = 0;
+    let user: { id: string, name: string, imageUrl: string, email: string, id_token: string } = undefined;
+    let loadingAuthState = true;
+    let expirationTime = undefined;
+    let currentTime = Math.floor(Date.now() / 1000);
+    let madeFirstRequest = false;
 
-	setInterval(() => {
-		currentTime = Math.floor(Date.now() / 1000);
-	}, 500)
+    // update current time global variable every 500 milliseconds
+    setInterval(() => {
+        currentTime = Math.floor(Date.now() / 1000);
+    }, 500)
 
-	// check if a variable is an integer
-	const isInt = (value) => {
-		return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
-	}
+    // check if a variable is an integer
+    const isInt = (value) => {
+        return !isNaN(value) && (function (x) {
+            return (x | 0) === x;
+        })(parseFloat(value))
+    }
 
-	// send pixel to server
-	const submitPixel = (e) => {
-		e.preventDefault();
+    // handle form submission
+    const submitPixel = (e) => {
+        e.preventDefault();
+        drawPixel(xInput, yInput, redInput, greenInput, blueInput);
+    }
 
-		if (user === undefined || user.id_token === undefined) {
-			console.error("Invalid user");
-			return;
-		}
+    // send pixel to server
+    const drawPixel = (xInput, yInput, redInput, greenInput, blueInput) => {
+        console.log("Sending draw pixel request...");
 
-		if (xInput === undefined || yInput === undefined || redInput === undefined
-				|| greenInput === undefined || blueInput === undefined) {
-			console.error("Undefined input");
-			return;
-		}
+        if (user === undefined || user.id_token === undefined) {
+            console.error("Invalid user");
+            return;
+        }
 
-		if (!isInt(xInput) || !isInt(yInput) || !isInt(redInput)
-				|| !isInt(greenInput) || !isInt(blueInput)) {
-			console.error("Non-integer input");
-			return;
-		}
+        if (xInput === undefined || yInput === undefined || redInput === undefined
+            || greenInput === undefined || blueInput === undefined) {
+            console.error("Undefined input");
+            return;
+        }
 
-		let data = JSON.stringify({
-			"x": xInput,
-			"y": yInput,
-			"red": redInput,
-			"green": greenInput,
-			"blue": blueInput
-		});
+        if (!isInt(xInput) || !isInt(yInput) || !isInt(redInput)
+            || !isInt(greenInput) || !isInt(blueInput)) {
+            console.error("Non-integer input");
+            return;
+        }
 
-		let config = {
-			method: 'post',
-			url: `${serverUrl}/set_pixel`,
-			headers: {
-				'Authorization': `Bearer ${user.id_token.toString()}`,
-				'Content-Type': 'application/json'
-			},
-			data : data
-		};
+        let data = JSON.stringify({
+            "x": xInput,
+            "y": yInput,
+            "red": redInput,
+            "green": greenInput,
+            "blue": blueInput
+        });
 
-		axios(config)
-				.then(function (response) {
-					if (response?.data?.status === "success") {
-						console.log(response?.data);
-						location.reload();
-					} else if (response?.data?.status === "ratelimit") {
-						console.log(response?.data);
-						expirationTime = response?.data?.message;
+        let config = {
+            method: 'post',
+            url: `${serverUrl}/set_pixel`,
+            headers: {
+                'Authorization': `Bearer ${user.id_token.toString()}`,
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
 
-						if (expirationTime !== undefined) {
-							expirationTime = parseInt(expirationTime)
-						}
-					} else {
-						console.log(response?.data);
-					}
-				})
-				.catch(function (error) {
-					console.log(error);
-				});
-	}
+        axios(config)
+            .then(function (response) {
+                if (response?.data?.status === "success") {
+                    console.log(response?.data);
+                    location.reload();
+                } else if (response?.data?.status === "ratelimit") {
+                    console.log(response?.data);
+                    expirationTime = response?.data?.message;
 
-	// when user signs in
-	const onSuccess = (googleUser) => {
-		console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-		const profile = googleUser.getBasicProfile();
+                    if (expirationTime !== undefined) {
+                        expirationTime = parseInt(expirationTime)
+                    }
+                } else {
+                    console.log(response?.data);
+                }
 
-		user = {
-			id: profile.getId(),
-			name: profile.getName(),
-			imageUrl: profile.getImageUrl(),
-			email: profile.getEmail(),
-			id_token: googleUser.getAuthResponse().id_token
-		}
-	}
+                madeFirstRequest = true;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
-	// when unable to sign in user
-	const onFailure = (error) => {
-		console.log("Failure for login", error);
-		user = undefined;
-	}
+    // when user signs in
+    const onSuccess = (googleUser) => {
+        console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+        const profile = googleUser.getBasicProfile();
 
-	// when auth has loaded
-	(window as any).onLoadCallback = () => {
-		console.log("Auth state loaded");
+        user = {
+            id: profile.getId(),
+            name: profile.getName(),
+            imageUrl: profile.getImageUrl(),
+            email: profile.getEmail(),
+            id_token: googleUser.getAuthResponse().id_token
+        }
 
-		// @ts-ignore
-		gapi.load('auth2', function() {
-			// @ts-ignore
-			gapi.auth2.init({
-				client_id: "512758578665-7mrm2s66ub7rd983qu3sv51ohgh79pqh.apps.googleusercontent.com"
-			}).then(() => {
-				loadingAuthState = false;
-			})
-		});
+        // initiate an invalid draw to display timer
+        drawPixel(-1, -1, 0, 0, 0);
+    }
 
-		// @ts-ignore
-		gapi.signin2.render('my-signin2', {
-			'longtitle': true,
-			'theme': 'light',
-			'onsuccess': onSuccess,
-			'onfailure': onFailure
-		});
-	}
+    // when unable to sign in user
+    const onFailure = (error) => {
+        console.log("Failure for login", error);
+        user = undefined;
+    }
 
-	// function to sign out
-	const signOut = () => {
-		// @ts-ignore
-		const auth2 = gapi.auth2.getAuthInstance();
-		auth2.signOut().then(() => {
-			console.log("Signed out");
-			user = undefined;
-			location.reload();
-		});
-	}
+    // when auth has loaded
+    (window as any).onLoadCallback = () => {
+        console.log("Auth state loaded");
+
+        // @ts-ignore
+        gapi.load('auth2', function () {
+            // @ts-ignore
+            gapi.auth2.init({
+                client_id: "512758578665-7mrm2s66ub7rd983qu3sv51ohgh79pqh.apps.googleusercontent.com"
+            }).then(() => {
+                loadingAuthState = false;
+            })
+        });
+
+        // @ts-ignore
+        gapi.signin2.render('my-signin2', {
+            'width': 300,
+            'longtitle': true,
+            'theme': 'light',
+            'onsuccess': onSuccess,
+            'onfailure': onFailure
+        });
+    }
+
+    // function to sign out
+    const signOut = () => {
+        // @ts-ignore
+        const auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(() => {
+            console.log("Signed out");
+            user = undefined;
+            location.reload();
+        });
+    }
 </script>
 
 <svelte:head>
-	<script src="https://apis.google.com/js/platform.js?onload=onLoadCallback" async defer></script>
+    <script src="https://apis.google.com/js/platform.js?onload=onLoadCallback" async defer></script>
 </svelte:head>
 
 <main>
-	<img id="output_image" alt="Reddit place canvas." src={imageUrl}/>
+    <div class="container">
+        <img id="output_image" alt="Reddit place canvas." src={imageUrl}/>
 
-	{#if user !== undefined && loadingAuthState === false}
-		<form on:submit|preventDefault={submitPixel}>
-			<label>
-				X
-				<input type="number" id="x-input" bind:value={xInput}/>
-			</label>
-			<label>
-				Y
-				<input type="number" id="y-input" bind:value={yInput}/>
-			</label>
-			<label>
-				Red
-				<input type="number" id="red-input" bind:value={redInput}/>
-			</label>
-			<label>
-				Green
-				<input type="number" id="green-input" bind:value={greenInput}/>
-			</label>
-			<label>
-				Blue
-				<input type="number" id="blue-input" bind:value={blueInput}/>
-			</label>
-			<button type="submit" onsubmit="submitPixel(e)">Set Pixel</button>
-		</form>
+        {#if expirationTime !== undefined && isInt(expirationTime) && (expirationTime - currentTime) >= 0}
+            <p>{expirationTime - currentTime} seconds until you can place a new pixel</p>
+        {:else if (user && !loadingAuthState && madeFirstRequest)}
+            <form on:submit|preventDefault={submitPixel} class="form">
+                <div>
+                    <label for="x-input">X</label>
+                    <input type="number" id="x-input" bind:value={xInput}/>
+                </div>
 
-		<a href="javascript:void(0);" on:click={signOut}>Sign out</a>
-	{/if}
+                <div>
+                    <label for="y-input">Y</label>
+                    <input type="number" id="y-input" bind:value={yInput}/>
+                </div>
 
-	{#if expirationTime !== undefined && isInt(expirationTime) && expirationTime - currentTime >= 0}
-		<p>{expirationTime - currentTime} seconds until you can place a new pixel</p>
-	{/if}
+                <div>
+                    <label for="red-input">Red</label>
+                    <input type="number" id="red-input" bind:value={redInput}/>
+                </div>
 
-	<div id="my-signin2" style={`visibility: ${(user === undefined && loadingAuthState === false) ? 'visible' : 'hidden'}`}></div>
+                <div>
+                    <label for="green-input">Green</label>
+                    <input type="number" id="green-input" bind:value={greenInput}/>
+                </div>
+
+                <div>
+                    <label for="blue-input">Blue</label>
+                    <input type="number" id="blue-input" bind:value={blueInput}/>
+                </div>
+
+                <button type="submit">Set Pixel</button>
+            </form>
+        {/if}
+
+        <div class="auth-container">
+            {#if (user && !loadingAuthState && madeFirstRequest)}
+                <a href="javascript:void(0);" on:click={signOut}>Sign out</a>
+            {/if}
+
+            <div id="my-signin2"
+                 style={`visibility: ${(user === undefined && loadingAuthState === false) ? 'visible' : 'hidden'}`}></div>
+        </div>
+    </div>
 </main>
 
 <style>
-	img {
-		border: dotted black;
-		width: 300px;
-		height: 300px;
-		image-rendering: pixelated;
-		image-rendering: -moz-crisp-edges;
-		image-rendering: crisp-edges;
-	}
-	
-	a:hover {
-		cursor: pointer;
-	}
+    img {
+        border: dotted black;
+        width: 300px;
+        height: 300px;
+        image-rendering: pixelated;
+        image-rendering: -moz-crisp-edges;
+        image-rendering: crisp-edges;
+    }
+
+    a:hover {
+        cursor: pointer;
+    }
+
+    .container {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+    }
+
+    .auth-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        align-content: center;
+    }
 </style>
